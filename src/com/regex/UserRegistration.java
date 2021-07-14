7c0378cd859a232e8ce7d70acf6ffc627669b0fc
Chandan Tiwary <ctiwary36@gmail.com>
2021-07-15 02:42:06 +0530

    Added:UC3 email validation

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -10,7 +10,10 @@ public class UserRegistration {
     public boolean lastNameValidation(String firstName) {
         return Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", firstName);
     }
-    
+
+    public boolean emailValidation(String email) {
+        return Pattern.matches("^a-zA-Z0-9.]+@[a-zA-Z0-9.]+$", email);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         UserRegistration person= new UserRegistration();
@@ -18,8 +21,11 @@ public class UserRegistration {
         String firstName = scanner.nextLine();
         System.out.println("enter last name");
         String lastName = scanner.nextLine();
+        System.out.println("enter Email");
+        String email = scanner.nextLine();
         System.out.println("Welcome " +firstName+ " " +lastName+ " to the User Registration Program");
         System.out.println(person.firstNameValidation(firstName));
         System.out.println(person.lastNameValidation(lastName));
+        System.out.println(person.emailValidation(lastName));
     }
 }
