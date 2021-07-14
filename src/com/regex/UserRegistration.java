d072a864c1bc64b639a9660b2c4fb4a61738851e
Chandan Tiwary <ctiwary36@gmail.com>
2021-07-15 02:31:38 +0530

    Added:UC2 last name validation

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -10,7 +10,7 @@ public class UserRegistration {
     public boolean lastNameValidation(String firstName) {
         return Pattern.matches("^[A-Z]{1}+[a-z]{3,}$", firstName);
     }
-
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         UserRegistration person= new UserRegistration();
@@ -20,6 +20,6 @@ public class UserRegistration {
         String lastName = scanner.nextLine();
         System.out.println("Welcome " +firstName+ " " +lastName+ " to the User Registration Program");
         System.out.println(person.firstNameValidation(firstName));
-        System.out.println(person.firstNameValidation(lastName));
+        System.out.println(person.lastNameValidation(lastName));
     }
 }
